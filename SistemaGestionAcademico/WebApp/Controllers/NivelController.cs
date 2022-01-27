95fed0edf0e2756f0d234fee6bf30247628276c4
vmejiaec <vmejiaec@gmail.com>
2022-01-27 18:24:22 -0500

    Controller de Nivel

---
+++ SistemaGestionAcademico/WebApp/Controllers/NivelController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Modelo.Entidades;
+using ModeloDB;
+using System.Collections.Generic;
+
+namespace WebApp.Controllers
+{
+    public class NivelController : Controller
+    {
+        public AcademiaDB db { get; set; }
+
+        public NivelController(AcademiaDB db)
+        {
+            this.db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Nivel> listaNiveles = db.niveles.
+                Include(nivel => nivel.Materia);
+
+            return View(listaNiveles);
+        }
+    }
+}
