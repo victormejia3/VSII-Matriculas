c076d6f8a350fbe2e5b01e84d3fecb27f7033c0e
vmejiaec <vmejiaec@gmail.com>
2022-01-27 20:02:04 -0500

    Páginas Create, Delete, Edit

--- SistemaGestionAcademico/WebSGA/Controllers/MateriasController.cs
+++ SistemaGestionAcademico/WebSGA/Controllers/MateriasController.cs
@@ -22,19 +22,66 @@ namespace WebSGA.Controllers
             return View(listaMaterias);
         }
 
+        // Creación de una nueva materia
+        //  - Enviar a un formulario vacio
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
         }
-
-        public IActionResult Edit()
+        //  - Grabar una materia
+        [HttpPost]
+        public IActionResult Create( Materia materia )
         {
-            return View();
+            db.materias.Add(materia);
+            db.SaveChanges();
+
+            TempData["mensaje"] = $"La materia {materia.Nombre} ha sido creada exitosamente";
+
+            return RedirectToAction("Index");
         }
 
-        public IActionResult Delete()
+        // Edición de una materia
+        //  - Enviar a un formulario con los datos de la materia seleccionada
+        [HttpGet]
+        public IActionResult Edit(int id)
         {
-            return View();
+            Materia materia = db.materias.Find(id);
+
+            return View(materia);
         }
+        //  - Actualizar una materia
+        [HttpPost]
+        public IActionResult Edit(Materia materia)
+        {
+            db.materias.Update(materia);
+            db.SaveChanges();
+
+            TempData["mensaje"] = $"La materia {materia.Nombre} ha sido actualizada exitosamente";
+
+            return RedirectToAction("Index");
+        }
+
+        // Borrado de una materia
+        //  - Enviar a un formulario con los datos de la materia a eliminar
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            Materia materia = db.materias.Find(id);
+
+            return View(materia);
+        }
+        //  - Eliminar una materia
+        [HttpPost]
+        public IActionResult Delete(Materia materia)
+        {
+            db.materias.Remove(materia);
+            db.SaveChanges();
+
+            TempData["mensaje"] = $"La materia ha sido eliminada exitosamente";
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
