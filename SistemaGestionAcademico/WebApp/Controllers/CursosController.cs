0275fcdeeca5c4975ed867b1c25e31bec697ea9e
vmejiaec <vmejiaec@gmail.com>
2022-01-28 15:40:54 -0500

    Página de cursos, el listado

---
+++ SistemaGestionAcademico/WebApp/Controllers/CursosController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Modelo.Entidades;
+using ModeloDB;
+using System.Collections.Generic;
+
+namespace WebApp.Controllers
+{
+    public class CursosController : Controller
+    {
+        readonly AcademiaDB db;
+
+        public CursosController(AcademiaDB db)
+        {
+            this.db = db;
+        }
+
+
+        // Obtiene la lista de cursos y lo envía a la vista
+        public IActionResult Index()
+        {
+            IEnumerable<Curso> listaCursos = db.cursos;
+
+            return View(listaCursos);
+        }
+    }
+}
